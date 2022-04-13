b634b9f514f8779b4f34bb784dec1fb99710b7ec
mensur056 <mansur.sarxanov@gmail.com>
2022-04-13 17:56:23 +0400

    'test'

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const double iconSize = 30;
+
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -12,47 +14,69 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          toolbarHeight: 90,
-          title: Image.asset(
-            'assets/images/im_youtube.png',
-            width: 150,
-            height: 70,
+        appBar: buildAppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ],
           ),
-          actions: [
-            const Icon(
-              Icons.add_to_queue_sharp,
-              color: Colors.grey,
-            ),
-            const SizedBox(
-              width: 15,
-            ),
-            const Icon(
-              Icons.notifications,
-              color: Colors.grey,
-            ),
-            const SizedBox(
-              width: 15,
-            ),
-            const Icon(
-              Icons.search,
-              color: Colors.grey,
-            ),
-            const SizedBox(
-              width: 15,
-            ),
-            CircleAvatar(
-              backgroundColor: Colors.purpleAccent,
-              radius: 20,
-              child: Image.asset('assets/icons/ic_profile.png'),
-            ),
-            const SizedBox(
-              width: 15,
-            ),
-          ],
         ),
       ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      toolbarHeight: 80,
+      title: Image.asset(
+        'assets/images/im_youtube.png',
+        width: 110,
+      ),
+      actions: [
+        const Icon(
+          Icons.add_to_queue_sharp,
+          color: Colors.black,
+          size: iconSize,
+        ),
+        buildSizedBox(),
+        const Icon(
+          Icons.notifications,
+          color: Colors.black,
+          size: iconSize,
+        ),
+        buildSizedBox(),
+        const Icon(
+          Icons.search,
+          color: Colors.black,
+          size: iconSize,
+        ),
+        buildSizedBox(),
+        CircleAvatar(
+          backgroundColor: Colors.purpleAccent,
+          radius: 16,
+          child: Image.asset('assets/icons/ic_profile.png'),
+        ),
+        buildSizedBox(),
+      ],
+    );
+  }
+
+  SizedBox buildSizedBox() {
+    return const SizedBox(
+      width: 20,
     );
   }
 }
