dd127b4cc56789061fb4f8e4db2eb272c325f982
mensur056 <mansur.sarxanov@gmail.com>
2022-04-13 14:21:23 +0400

    'test'

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  final String appName = 'Youtube';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
+  }
+}
