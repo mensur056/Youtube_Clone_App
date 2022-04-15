da78bdaa692e7abae202578391bc1404fb9d4428
mensur056 <mansur.sarxanov@gmail.com>
2022-04-15 18:38:09 +0400

    'test'

---
+++ lib/models/video_model.dart
@@ -0,0 +1,12 @@
+class VideoModel {
+  String? thumnail;
+  String? videoTitle;
+  String? videoDuration;
+  String? avatar;
+  String? channel;
+  String? views;
+  String? date;
+
+  VideoModel(this.avatar, this.channel, this.date, this.thumnail,
+      this.videoDuration, this.videoTitle, this.views);
+}
